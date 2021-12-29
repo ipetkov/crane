46e6e54b3ba98bc94c80d6e128fca51628af094f
Ivan Petkov <ivanppetkov@gmail.com>
2021-12-28 16:15:57 -0800

    Update inheritCargoTargetHook to run after source patching
    * This allows us to copy over the cargo artifacts as early as possible
      so that any other phases/hooks (like running clippy) can take
      advantage of the cache
    * It also avoids installing the artifacts too soon (and making them
      older than the source) and having cargo invalidate them

--- pkgs/inheritCargoTargetHook.sh
+++ pkgs/inheritCargoTargetHook.sh
@@ -24,7 +24,11 @@ inheritCargoTarget() {
 }
 
 if [ -n "${inheritCargoTarget-}" ]; then
-  preBuildHooks+=(inheritCargoTarget)
+  # NB: inherit cargo artifacts after patching is done, that way target directory
+  # is fresher than the source and avoid invalidating the cache
+  # Doing this as early as possible also gives us the advantage that any other
+  # preBuild hooks (e.g. clippy) can also take advantage of the cache
+  postPatchHooks+=(inheritCargoTarget)
 else
-  echo "inheritCargoTarget not set"
+  echo "inheritCargoTarget not set, will not reuse any cargo artifacts"
 fi
