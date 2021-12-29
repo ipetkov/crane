7b80c23eb5919ded5d28f8d728c5ac01b209a4af
Ivan Petkov <ivanppetkov@gmail.com>
2021-12-28 16:05:20 -0800

    Update configureCargoVendoredDepsHook to use $CARGO_HOME/config.toml
    * Rather than clobbering the project's .cargo/config.toml file (if it
      exists), the setup hook will update the base configuration used in
      $CARGO_HOME/config.toml
    * This potentially allows for projects who know what they are doing to
      more easily replace their own sources, while falling back to our
      configuration

--- pkgs/configureCargoVendoredDepsHook.sh
+++ pkgs/configureCargoVendoredDepsHook.sh
@@ -1,20 +1,23 @@
 configureCargoVendoredDepsHook() {
   echo "Executing configureCargoVendoredDepsHook"
 
-  mkdir -p .cargo
-  cat >>.cargo/config <<EOF
+  # NB: we add this configuration to $CARGO_HOME/config.toml
+  # instead of .cargo/config.toml. This allows cargo to honor the
+  # project's configuration (if it exists) with greater specificity
+  # than the config we are adding. If the project knows what it is
+  # doing and has its own source replacement going on, it can happily
+  # ignore the changes we are trying to make!
+  cat >>${CARGO_HOME:?not defined}/config.toml <<EOF
 [source.crates-io]
 replace-with = "nix-sources"
 
 [source.nix-sources]
 directory = "${cargoVendorDir}"
 EOF
-
-  echo "Finished configureCargoVendoredDepsHook"
 }
 
 if [ -n "${cargoVendorDir-}" ]; then
   preConfigureHooks+=(configureCargoVendoredDepsHook)
 else
-  echo "cargoVendorDir not set"
+  echo "cargoVendorDir not set, will not automatically configure vendored sources"
 fi
