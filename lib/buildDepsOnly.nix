263fe7c78fef61137fb81b53a89ffb55d7c90875
Ivan Petkov <ivanppetkov@gmail.com>
2022-01-08 16:57:45 -0800

    Fix operator precedence typo
    * In Nix the `or` operator has higher precedence than function
      application, therefore `x or y z` parses as `(x or y) z`

--- lib/buildDepsOnly.nix
+++ lib/buildDepsOnly.nix
@@ -26,7 +26,7 @@ mkCargoDerivation (cleanedArgs // {
   version = args.version or crateName.version;
 
   cargoArtifacts = null;
-  cargoVendorDir = args.cargoVendorDir or vendorCargoDepsFromArgs args;
+  cargoVendorDir = args.cargoVendorDir or (vendorCargoDepsFromArgs args);
 
   # First we run `cargo check` to cache cargo's internal artifacts, fingerprints, etc. for all deps.
   # Then we run `cargo build` to actually compile the deps and cache the results
