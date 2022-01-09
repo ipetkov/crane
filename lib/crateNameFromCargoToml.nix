72bb21ee86570d3e695ae80df0be53d32f911e31
Ivan Petkov <ivanppetkov@gmail.com>
2022-01-09 15:49:06 -0800

    crateNameFromCargoToml: fix parameter validation
    * Previously the check was overzealous and it was impossible to only
      specify `cargoToml` (since both other parameters would be null)
    * Instead, we take advantage of Nix's laziness and only throw when
      needed

--- lib/crateNameFromCargoToml.nix
+++ lib/crateNameFromCargoToml.nix
@@ -1,27 +1,18 @@
 { fromTOML }:
 
-{ src ? null
-, cargoToml ? src + /Cargo.toml
-, cargoTomlContents ? null
-, ...
-}:
-
-if cargoTomlContents == null && (src == null || !builtins.pathExists cargoToml)
-then
-  throw ''
+args:
+let
+  src = args.src or (throw ''
     unable to infer crate name and version. Please make sure the src directory
     contains a valid Cargo.toml file, or consider setting a derivation name explicitly
-  ''
-else
-  let
-    cargoTomlRealContents =
-      if cargoTomlContents != null
-      then cargoTomlContents
-      else builtins.readFile cargoToml;
+  '');
 
-    toml = fromTOML cargoTomlRealContents;
-  in
-  {
-    pname = toml.package.name or "cargo-package";
-    version = toml.package.version or "unknown";
-  }
+  cargoToml = args.cargoToml or (args.src + /Cargo.toml);
+  cargoTomlContents = args.cargoTomlContents or (builtins.readFile cargoToml);
+
+  toml = fromTOML cargoTomlContents;
+in
+{
+  pname = toml.package.name or "cargo-package";
+  version = toml.package.version or "unknown";
+}
