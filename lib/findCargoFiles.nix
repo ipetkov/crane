5bae4b91c1a5554a82ca49dc0d5cbe9def19d654
Ivan Petkov <ivanppetkov@gmail.com>
2022-01-28 18:24:51 -0800

    Optimize searching for cargo configuration files
    * Rather than recursively look up all paths in a given project and then
      filtering for cargo-specific files, we now use our own customized
      traversal method to only capture the results we want.

---
+++ lib/findCargoFiles.nix
@@ -0,0 +1,41 @@
+{ lib
+}:
+
+src:
+let
+  inherit (lib)
+    flatten
+    lists
+    mapAttrs
+    mapAttrsToList;
+
+  # A specialized form of lib.listFilesRecursive except it will only look
+  # for Cargo.toml and config.toml files to keep the intermediate results lean
+  listFilesRecursive = parentIsDotCargo: dir: flatten (mapAttrsToList
+    (name: type:
+      let
+        cur = dir + "/${name}";
+        isConfig = parentIsDotCargo && (name == "config" || name == "config.toml");
+        isCargoToml = name == "Cargo.toml";
+      in
+      if type == "directory"
+      then listFilesRecursive (name == ".cargo") cur
+      else if isCargoToml
+      then [{ path = cur; type = "cargoTomls"; }]
+      else if isConfig
+      then [{ path = cur; type = "cargoConfigs"; }]
+      else [ ]
+    )
+    (builtins.readDir dir));
+
+  foundFiles = listFilesRecursive false src;
+  grouped = builtins.groupBy (x: x.type) foundFiles;
+  cleaned = mapAttrs (_: map (y: y.path)) grouped;
+
+  # Ensure we have a well typed result
+  default = {
+    cargoTomls = [ ];
+    cargoConfigs = [ ];
+  };
+in
+default // cleaned
