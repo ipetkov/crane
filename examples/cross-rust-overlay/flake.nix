a185f3753a176f5a20caa7a1e2c900427422e700
Ivan Petkov <ivanppetkov@gmail.com>
2022-02-24 17:49:22 -0800

    Update cross compile example
    * Sometimes we also need to set the HOST_CC environment variable to
      allow cross compiling crates which also need to build a program to run
      on the build platform itself

--- examples/cross-rust-overlay/flake.nix
+++ examples/cross-rust-overlay/flake.nix
@@ -88,7 +88,18 @@
             # See: https://doc.rust-lang.org/cargo/reference/config.html#target
             CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
             CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
+
+            # Tell cargo which target we want to build (so it doesn't default to the build system).
+            # We can either set a cargo flag explicitly with a flag or with an environment variable.
             cargoExtraArgs = "--target aarch64-unknown-linux-gnu";
+            # CARGO_BUILD_TARGET = "aarch64-unknown-linux-gnu";
+
+            # This environment variable may be necessary if any of your dependencies use a
+            # build-script which invokes the `cc` crate to build some other code. The `cc` crate
+            # should automatically pick up on our target-specific linker above, but this may be
+            # necessary if the build script needs to compile and run some extra code on the build
+            # system.
+            HOST_CC = "${stdenv.cc.nativePrefix}cc";
           };
 
         # Assuming the above expression was in a file called myCrate.nix
