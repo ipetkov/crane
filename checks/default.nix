9eb8357f53cc2bff06b0727e6560f6a8fd6759b0
Ivan Petkov <ivanppetkov@gmail.com>
2022-07-09 20:20:15 -0700

    Reformat nix files

--- checks/default.nix
+++ checks/default.nix
@@ -5,183 +5,183 @@ let
   onlyDrvs = lib.filterAttrs (_: lib.isDerivation);
 in
 onlyDrvs (lib.makeScope myLib.newScope (self:
-  let
-    callPackage = self.newScope { };
-    x64Linux = pkgs.hostPlatform.system == "x86_64-linux";
-  in
-  myPkgs // {
-    checkNixpkgsFmt = callPackage ./nixpkgs-fmt.nix { };
+let
+  callPackage = self.newScope { };
+  x64Linux = pkgs.hostPlatform.system == "x86_64-linux";
+in
+myPkgs // {
+  checkNixpkgsFmt = callPackage ./nixpkgs-fmt.nix { };
 
-    cleanCargoTomlTests = callPackage ./cleanCargoTomlTests { };
+  cleanCargoTomlTests = callPackage ./cleanCargoTomlTests { };
 
-    clippy = callPackage ./clippy { };
+  clippy = callPackage ./clippy { };
 
-    cargoFmt = myLib.cargoFmt {
-      src = ./simple;
-    };
+  cargoFmt = myLib.cargoFmt {
+    src = ./simple;
+  };
 
-    # https://github.com/ipetkov/crane/issues/6
-    cargoClippyThenBuild = myLib.buildPackage {
-      src = ./simple;
-      cargoArtifacts = myLib.cargoClippy {
-        cargoArtifacts = null;
-        src = ./simple;
-      };
-    };
-
-    # https://github.com/ipetkov/crane/issues/6
-    cargoFmtThenClippy = myLib.cargoClippy {
-      src = ./simple;
-      cargoArtifacts = self.cargoFmt;
-    };
-
-    cargoTarpaulin = lib.optionalAttrs x64Linux (myLib.cargoTarpaulin {
-      src = ./simple;
-    });
-
-    compilesFresh = callPackage ./compilesFresh.nix { };
-    compilesFreshSimple = self.compilesFresh ./simple "simple" { };
-    compilesFreshOverlappingTargets = self.compilesFresh
-      ./overlapping-targets
-      (builtins.concatStringsSep "\n" [
-        "bar"
-        "baz"
-        "foo"
-        "overlapping-targets"
-      ])
-      { };
-
-    customCargoTargetDirectory =
-      let
-        simple = self.simple.overrideAttrs (old: {
-          pname = "customCargoTargetDirectory";
-          doInstallCargoArtifacts = false;
-          CARGO_TARGET_DIR = "some/nested/custom-cargo-dir";
-        });
-      in
-      pkgs.runCommand "smoke-simple" { } ''
-        # does it run?
-        ${simple}/bin/simple
-        touch $out
-      '';
-
-    depsOnlyVariousTargets = myLib.buildDepsOnly {
-      src = ./various-targets;
-    };
-
-    features = callPackage ./features { };
-
-    gitOverlappingRepo = myLib.buildPackage {
-      src = ./git-overlapping;
-    };
-
-    gitRevNoRef = myLib.buildPackage {
-      src = ./gitRevNoRef;
-    };
-
-    illegalBin = myLib.buildPackage {
-      pname = "illegalBin";
-      version = "0.0.1";
-      src = ./illegal-bin;
-    };
-
-    manyLibs = myLib.buildPackage {
-      src = ./with-libs;
-      pname = "my-libs";
-      version = "0.0.1";
+  # https://github.com/ipetkov/crane/issues/6
+  cargoClippyThenBuild = myLib.buildPackage {
+    src = ./simple;
+    cargoArtifacts = myLib.cargoClippy {
       cargoArtifacts = null;
+      src = ./simple;
     };
+  };
 
-    manyLibsInstalledAsExpected = pkgs.runCommand "manyLibsInstalledAsExpected" { } ''
-      cat >expected <<EOF
-      liball_types.a
-      liball_types.so
-      libonly_cdylib.so
-      libonly_staticlib.a
-      EOF
+  # https://github.com/ipetkov/crane/issues/6
+  cargoFmtThenClippy = myLib.cargoClippy {
+    src = ./simple;
+    cargoArtifacts = self.cargoFmt;
+  };
 
-      diff ./expected <(ls -1 ${self.manyLibs}/lib)
+  cargoTarpaulin = lib.optionalAttrs x64Linux (myLib.cargoTarpaulin {
+    src = ./simple;
+  });
+
+  compilesFresh = callPackage ./compilesFresh.nix { };
+  compilesFreshSimple = self.compilesFresh ./simple "simple" { };
+  compilesFreshOverlappingTargets = self.compilesFresh
+    ./overlapping-targets
+    (builtins.concatStringsSep "\n" [
+      "bar"
+      "baz"
+      "foo"
+      "overlapping-targets"
+    ])
+    { };
+
+  customCargoTargetDirectory =
+    let
+      simple = self.simple.overrideAttrs (old: {
+        pname = "customCargoTargetDirectory";
+        doInstallCargoArtifacts = false;
+        CARGO_TARGET_DIR = "some/nested/custom-cargo-dir";
+      });
+    in
+    pkgs.runCommand "smoke-simple" { } ''
+      # does it run?
+      ${simple}/bin/simple
       touch $out
     '';
 
-    mkDummySrcTests = callPackage ./mkDummySrcTests { };
+  depsOnlyVariousTargets = myLib.buildDepsOnly {
+    src = ./various-targets;
+  };
 
-    simple = myLib.buildPackage {
-      src = ./simple;
-    };
-    simpleGit = myLib.buildPackage {
-      src = ./simple-git;
-    };
+  features = callPackage ./features { };
 
-    simple-nonflake = (import ../default.nix {
-      inherit pkgs;
-    }).buildPackage {
-      src = ./simple;
-    };
+  gitOverlappingRepo = myLib.buildPackage {
+    src = ./git-overlapping;
+  };
 
-    remapPathPrefixWorks = pkgs.runCommand "remapPathPrefixWorks" { } ''
-      if ${pkgs.binutils-unwrapped}/bin/strings ${self.ripgrep}/bin/rg | \
-        grep -v glibc | \
-        grep --count '/nix/store'
-      then
-        echo found references to /nix/store sources
-        false
-      else
-        touch $out
-      fi
-    '';
+  gitRevNoRef = myLib.buildPackage {
+    src = ./gitRevNoRef;
+  };
 
-    # Test building a real world example
-    ripgrep = myLib.buildPackage {
-      inherit (pkgs.ripgrep) pname src version;
-    };
+  illegalBin = myLib.buildPackage {
+    pname = "illegalBin";
+    version = "0.0.1";
+    src = ./illegal-bin;
+  };
 
-    smoke = callPackage ./smoke.nix { };
-    smokeSimple = self.smoke [ "simple" ] self.simple;
-    smokeSimpleGit = self.smoke [ "simple-git" ] self.simpleGit;
-    smokeAltRegistry = self.smoke [ "alt-registry" ] (
-      let
-        myLibWithRegistry = myLib.appendCrateRegistries [
-          (myLib.registryFromGitIndex {
-            indexUrl = "https://github.com/Hirevo/alexandrie-index";
-            rev = "90df25daf291d402d1ded8c32c23d5e1498c6725";
-          })
-        ];
-      in
-      myLibWithRegistry.buildPackage {
-        src = ../examples/alt-registry;
-        nativeBuildInputs = with pkgs; [
-          pkgconfig
-          openssl
-        ];
-      }
-    );
+  manyLibs = myLib.buildPackage {
+    src = ./with-libs;
+    pname = "my-libs";
+    version = "0.0.1";
+    cargoArtifacts = null;
+  };
 
-    smokeOverlappingTargets = self.smoke [ "foo" "bar" "baz" ] (myLib.buildPackage {
-      src = ./overlapping-targets;
-    });
+  manyLibsInstalledAsExpected = pkgs.runCommand "manyLibsInstalledAsExpected" { } ''
+    cat >expected <<EOF
+    liball_types.a
+    liball_types.so
+    libonly_cdylib.so
+    libonly_staticlib.a
+    EOF
 
-    smokeManuallyVendored = self.smoke [ "manually-vendored" ] (myLib.buildPackage {
-      src = ./manually-vendored;
-      cargoVendorDir = ./manually-vendored/vendor;
-    });
+    diff ./expected <(ls -1 ${self.manyLibs}/lib)
+    touch $out
+  '';
 
-    smokeWorkspace = self.smoke [ "print" ] self.workspace;
-    smokeWorkspaceRoot = self.smoke [ "print" ] self.workspaceRoot;
+  mkDummySrcTests = callPackage ./mkDummySrcTests { };
 
-    workspace = myLib.buildPackage {
-      src = ./workspace;
-      pname = "workspace";
-    };
+  simple = myLib.buildPackage {
+    src = ./simple;
+  };
+  simpleGit = myLib.buildPackage {
+    src = ./simple-git;
+  };
 
-    workspaceRoot = myLib.buildPackage {
-      src = ./workspace-root;
-      pname = "workspace-root";
-    };
+  simple-nonflake = (import ../default.nix {
+    inherit pkgs;
+  }).buildPackage {
+    src = ./simple;
+  };
 
-    workspaceGit = myLib.buildPackage {
-      src = ./workspace-git;
-      pname = "workspace-git";
-    };
-  })
+  remapPathPrefixWorks = pkgs.runCommand "remapPathPrefixWorks" { } ''
+    if ${pkgs.binutils-unwrapped}/bin/strings ${self.ripgrep}/bin/rg | \
+      grep -v glibc | \
+      grep --count '/nix/store'
+    then
+      echo found references to /nix/store sources
+      false
+    else
+      touch $out
+    fi
+  '';
+
+  # Test building a real world example
+  ripgrep = myLib.buildPackage {
+    inherit (pkgs.ripgrep) pname src version;
+  };
+
+  smoke = callPackage ./smoke.nix { };
+  smokeSimple = self.smoke [ "simple" ] self.simple;
+  smokeSimpleGit = self.smoke [ "simple-git" ] self.simpleGit;
+  smokeAltRegistry = self.smoke [ "alt-registry" ] (
+    let
+      myLibWithRegistry = myLib.appendCrateRegistries [
+        (myLib.registryFromGitIndex {
+          indexUrl = "https://github.com/Hirevo/alexandrie-index";
+          rev = "90df25daf291d402d1ded8c32c23d5e1498c6725";
+        })
+      ];
+    in
+    myLibWithRegistry.buildPackage {
+      src = ../examples/alt-registry;
+      nativeBuildInputs = with pkgs; [
+        pkgconfig
+        openssl
+      ];
+    }
+  );
+
+  smokeOverlappingTargets = self.smoke [ "foo" "bar" "baz" ] (myLib.buildPackage {
+    src = ./overlapping-targets;
+  });
+
+  smokeManuallyVendored = self.smoke [ "manually-vendored" ] (myLib.buildPackage {
+    src = ./manually-vendored;
+    cargoVendorDir = ./manually-vendored/vendor;
+  });
+
+  smokeWorkspace = self.smoke [ "print" ] self.workspace;
+  smokeWorkspaceRoot = self.smoke [ "print" ] self.workspaceRoot;
+
+  workspace = myLib.buildPackage {
+    src = ./workspace;
+    pname = "workspace";
+  };
+
+  workspaceRoot = myLib.buildPackage {
+    src = ./workspace-root;
+    pname = "workspace-root";
+  };
+
+  workspaceGit = myLib.buildPackage {
+    src = ./workspace-git;
+    pname = "workspace-git";
+  };
+})
 )
--- lib/default.nix
+++ lib/default.nix
@@ -4,46 +4,46 @@
 }:
 
 lib.makeScope newScope (self:
-  let
-    inherit (self) callPackage;
-  in
-  (mkMyPkgs callPackage) // {
-    appendCrateRegistries = input: self.overrideScope' (final: prev: {
-      crateRegistries = prev.crateRegistries // (lib.foldl (a: b: a // b) { } input);
-    });
+let
+  inherit (self) callPackage;
+in
+(mkMyPkgs callPackage) // {
+  appendCrateRegistries = input: self.overrideScope' (final: prev: {
+    crateRegistries = prev.crateRegistries // (lib.foldl (a: b: a // b) { } input);
+  });
 
-    buildDepsOnly = callPackage ./buildDepsOnly.nix { };
-    buildPackage = callPackage ./buildPackage.nix { };
-    cargoBuild = callPackage ./cargoBuild.nix { };
-    cargoClippy = callPackage ./cargoClippy.nix { };
-    cargoFmt = callPackage ./cargoFmt.nix { };
-    cargoTarpaulin = callPackage ./cargoTarpaulin.nix { };
-    cleanCargoToml = callPackage ./cleanCargoToml.nix { };
-    crateNameFromCargoToml = callPackage ./crateNameFromCargoToml.nix { };
+  buildDepsOnly = callPackage ./buildDepsOnly.nix { };
+  buildPackage = callPackage ./buildPackage.nix { };
+  cargoBuild = callPackage ./cargoBuild.nix { };
+  cargoClippy = callPackage ./cargoClippy.nix { };
+  cargoFmt = callPackage ./cargoFmt.nix { };
+  cargoTarpaulin = callPackage ./cargoTarpaulin.nix { };
+  cleanCargoToml = callPackage ./cleanCargoToml.nix { };
+  crateNameFromCargoToml = callPackage ./crateNameFromCargoToml.nix { };
 
-    crateRegistries = self.registryFromDownloadUrl {
-      dl = "https://crates.io/api/v1/crates";
-      indexUrl = "https://github.com/rust-lang/crates.io-index";
-    };
+  crateRegistries = self.registryFromDownloadUrl {
+    dl = "https://crates.io/api/v1/crates";
+    indexUrl = "https://github.com/rust-lang/crates.io-index";
+  };
 
-    downloadCargoPackage = callPackage ./downloadCargoPackage.nix { };
-    downloadCargoPackageFromGit = callPackage ./downloadCargoPackageFromGit.nix { };
-    findCargoFiles = callPackage ./findCargoFiles.nix { };
-    mkCargoDerivation = callPackage ./mkCargoDerivation.nix { };
-    mkDummySrc = callPackage ./mkDummySrc.nix { };
+  downloadCargoPackage = callPackage ./downloadCargoPackage.nix { };
+  downloadCargoPackageFromGit = callPackage ./downloadCargoPackageFromGit.nix { };
+  findCargoFiles = callPackage ./findCargoFiles.nix { };
+  mkCargoDerivation = callPackage ./mkCargoDerivation.nix { };
+  mkDummySrc = callPackage ./mkDummySrc.nix { };
 
-    overrideToolchain = toolchain: self.overrideScope' (final: prev: {
-      cargo = toolchain;
-      clippy = toolchain;
-      rustc = toolchain;
-      rustfmt = toolchain;
-    });
+  overrideToolchain = toolchain: self.overrideScope' (final: prev: {
+    cargo = toolchain;
+    clippy = toolchain;
+    rustc = toolchain;
+    rustfmt = toolchain;
+  });
 
-    registryFromDownloadUrl = callPackage ./registryFromDownloadUrl.nix { };
-    registryFromGitIndex = callPackage ./registryFromGitIndex.nix { };
-    urlForCargoPackage = callPackage ./urlForCargoPackage.nix { };
-    vendorCargoDeps = callPackage ./vendorCargoDeps.nix { };
-    vendorCargoRegistries = callPackage ./vendorCargoRegistries.nix { };
-    vendorGitDeps = callPackage ./vendorGitDeps.nix { };
-    writeTOML = callPackage ./writeTOML.nix { };
-  })
+  registryFromDownloadUrl = callPackage ./registryFromDownloadUrl.nix { };
+  registryFromGitIndex = callPackage ./registryFromGitIndex.nix { };
+  urlForCargoPackage = callPackage ./urlForCargoPackage.nix { };
+  vendorCargoDeps = callPackage ./vendorCargoDeps.nix { };
+  vendorCargoRegistries = callPackage ./vendorCargoRegistries.nix { };
+  vendorGitDeps = callPackage ./vendorGitDeps.nix { };
+  writeTOML = callPackage ./writeTOML.nix { };
+})
