14fc4fc5cbbb4d2dc70806843d23bfd04ec4b0d0
N G <nicholas.gerstle@cognite.com>
2022-07-25 13:43:02 +0200

    accept advisory-db reference to cargo audit; format

--- lib/cargoAudit.nix
+++ lib/cargoAudit.nix
@@ -1,18 +1,19 @@
 { cargoBuild
 , cargo-audit
-}:
-
-{ cargoArtifacts
-, cargoAuditExtraArgs ? ""
-, cargoExtraArgs ? ""
-, ...
-}@origArgs:
+,
+}: { cargoArtifacts
+   , cargoAuditExtraArgs ? ""
+   , cargoExtraArgs ? ""
+   , advisory-db
+   , ...
+   } @ origArgs:
 let
   args = builtins.removeAttrs origArgs [ "cargoAuditExtraArgs" ];
 in
-cargoBuild (args // {
+cargoBuild (args
+  // {
   inherit cargoArtifacts;
-  cargoBuildCommand = "cargo audit";
+  cargoBuildCommand = "cargo audit -n -d ${advisory-db}";
   cargoExtraArgs = "${cargoExtraArgs} ${cargoAuditExtraArgs}";
 
   doCheck = false; # We don't need to run tests to benefit from `cargo audit`
