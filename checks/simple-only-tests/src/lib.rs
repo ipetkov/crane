9682b9f7e16b3e2f454a76a8106a0fc7ee002868
Ivan Petkov <ivanppetkov@gmail.com>
2022-08-10 17:19:07 -0700

    checks: add test case for building crate with only tests (#61)

---
+++ checks/simple-only-tests/src/lib.rs
@@ -0,0 +1,19 @@
+#[test]
+fn first() {
+    assert_eq!(1 + 1, 2);
+}
+
+#[test]
+fn second() {
+    assert_eq!(84 / 2, 42);
+}
+
+#[test]
+fn third() {
+    assert_eq!(5 * 5, 25);
+}
+
+#[test]
+fn fourth() {
+    assert_eq!(81 / 3, 27);
+}
