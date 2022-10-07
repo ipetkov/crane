7e4ed75955fd4c514d11340e682ad647a109301c
Roman Volosatovs <rvolosatovs@riseup.net>
2022-10-07 12:08:48 +0200

    mkDummySrc: support freestanding targets
    Currently, when building for targets that do not support `std`,
    like `x86_64-unknown-none`, `cargo check` on the costructed dummy source fails with:
    
    ```
     > error[E0463]: can't find crate for `std`
           >   |
           >   = note: the `x86_64-unknown-none` target may not support the standard library
           >   = note: `std` is required by `sallyport` because it does not declare `#![no_std]`
           >   = help: consider building the standard library from source with `cargo build -Zbuild-std`
           >
           > For more information about this error, try `rustc --explain E0463`.
    ```
    
    To prevent this issue from happening, set `#![no_std]` freestanding
    targets
    
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- checks/mkDummySrcTests/workspace/expected/member/foo/bar/baz/examples/example1.rs
+++ checks/mkDummySrcTests/workspace/expected/member/foo/bar/baz/examples/example1.rs
@@ -1,2 +1,9 @@
 #![allow(dead_code)]
+#![cfg_attr(target_os = "none", no_std)]
+
+#[cfg_attr(target_os = "none", panic_handler)]
+fn panic(_info: &::core::panic::PanicInfo<'_>) -> ! {
+    loop {}
+}
+
 pub fn main() {}
--- lib/mkDummySrc.nix
+++ lib/mkDummySrc.nix
@@ -122,6 +122,13 @@ let
 
   dummyrs = writeText "dummy.rs" ''
     #![allow(dead_code)]
+    #![cfg_attr(target_os = "none", no_std)]
+
+    #[cfg_attr(target_os = "none", panic_handler)]
+    fn panic(_info: &::core::panic::PanicInfo<'_>) -> ! {
+        loop {}
+    }
+
     pub fn main() {}
   '';
 
