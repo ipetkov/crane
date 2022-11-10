d8e658ecebdd1726690a97c2f51675410015165e
Ivan Petkov <ivanppetkov@gmail.com>
2022-11-09 17:47:41 -0800

    Disable logs for flake-cargo-git test

--- extra-tests/fetch-cargo-git/test.sh
+++ extra-tests/fetch-cargo-git/test.sh
@@ -6,7 +6,6 @@ scriptDir=$(dirname "$0")
 cd "${scriptDir}"
 
 craneOverride="--override-input crane ../.."
-flakeSrc=$(nix flake metadata ${craneOverride} --json 2>/dev/null | jq -r '.path')
 
 # Try fetching the git verision of cargo
-nix build ${craneOverride} .#cargo-git -L
+nix build ${craneOverride} .#cargo-git
