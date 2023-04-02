46624e9e2f31a63a5274604889a8b1dc22629d05
Ivan Petkov <ivanppetkov@gmail.com>
2023-04-02 10:44:21 -0700

    Don't build the build-std example with Nix 2.11

--- examples/build-std/flake.nix
+++ examples/build-std/flake.nix
@@ -21,7 +21,12 @@
   };
 
   outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    let
+      # Import from derivation has some issues in Nix 2.11 which were fixed with 2.12
+      isValidNixVersion = (builtins.compareVersions builtins.nixVersion "2.12") >= 0;
+      optionalList = cond: list: if cond then list else [ ];
+    in
+    flake-utils.lib.eachSystem (optionalList isValidNixVersion [ "x86_64-linux" ]) (system:
       let
         pkgs = import nixpkgs {
           inherit system;
