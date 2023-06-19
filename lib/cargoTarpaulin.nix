184f52f8d91a65e0bf2f1aa7a9a2194fa9be3e26
Ivan Petkov <ivanppetkov@gmail.com>
2023-06-19 13:08:30 -0700

    Revert unrelated change

--- lib/cargoTarpaulin.nix
+++ lib/cargoTarpaulin.nix
@@ -3,7 +3,7 @@
 }:
 
 { cargoExtraArgs ? ""
-, cargoTarpaulinExtraArgs ? "--out Xml --output-dir $out"
+, cargoTarpaulinExtraArgs ? "--skip-clean --out Xml --output-dir $out"
 , ...
 }@origArgs:
 let
