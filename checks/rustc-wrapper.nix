959fcf9a8a40fe9a47c33a402fa4052eb5d53eb6
Ivan Petkov <ivanppetkov@gmail.com>
2023-10-17 18:10:10 -0700

    Format

--- checks/rustc-wrapper.nix
+++ checks/rustc-wrapper.nix
@@ -4,11 +4,11 @@
 # A rustc wrapper which can conditionally refuse to re-build crates.
 # Useful for testing that artifact caching isn't somehow broken
 writeShellScript "rustc-wrapper" ''
-# Let through any cargo introspection calls
-if [[ -z "''${__CRANE_DENY_COMPILATION:-}" || "''${2:-}" == "-vV" || "''${4:-}" == "___" ]]; then
-  exec "$@"
-fi
+  # Let through any cargo introspection calls
+  if [[ -z "''${__CRANE_DENY_COMPILATION:-}" || "''${2:-}" == "-vV" || "''${4:-}" == "___" ]]; then
+    exec "$@"
+  fi
 
->&2 echo "recompilation forbiden: $*"
-exit 1
+  >&2 echo "recompilation forbiden: $*"
+  exit 1
 ''
