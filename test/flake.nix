81646638f9b2dca8bc80be8a4c66eb01d4958ecb
Ivan Petkov <ivanppetkov@gmail.com>
2023-10-19 00:30:52 +0000

    ci: pin all example inputs with a single testing flake.lock (#436)

---
+++ test/flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    # NB: nixpkgs-unstable testing will come from the root flake
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+
+    # The version of wasm-bindgen-cli needs to match the version in Cargo.lock
+    # Update this to include the version you need
+    nixpkgs-for-wasm-bindgen.url = "github:NixOS/nixpkgs/4e6868b1aa3766ab1de169922bb3826143941973";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+  };
+
+  outputs = _: { };
+}
