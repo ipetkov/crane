751290c945cb474288c4cc3cff48eff8379c76ae
Adam Curtis <adam.curtis.dev@gmail.com>
2024-01-26 21:36:30 -0500

    fix: add #[allow(unused_extern_crates)] to dummy.rs (#501)

--- checks/mkDummySrcTests/workspace-bindeps/expected/src/bin/crane-dummy-workspace-bindeps/main.rs
+++ checks/mkDummySrcTests/workspace-bindeps/expected/src/bin/crane-dummy-workspace-bindeps/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(any(target_os = "none", target_os = "uefi"), no_std)]
 #![cfg_attr(any(target_os = "none", target_os = "uefi"), no_main)]
 
+#[allow(unused_extern_crates)]
 extern crate core;
 
 #[cfg_attr(any(target_os = "none", target_os = "uefi"), panic_handler)]
--- lib/mkDummySrc.nix
+++ lib/mkDummySrc.nix
@@ -131,6 +131,7 @@ let
     #![cfg_attr(any(target_os = "none", target_os = "uefi"), no_std)]
     #![cfg_attr(any(target_os = "none", target_os = "uefi"), no_main)]
 
+    #[allow(unused_extern_crates)]
     extern crate core;
 
     #[cfg_attr(any(target_os = "none", target_os = "uefi"), panic_handler)]
