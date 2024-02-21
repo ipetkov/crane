45dd687083f3cfe4230a3037dc3dec37c3cd7a5c
Ivan Petkov <ivanppetkov@gmail.com>
2024-02-21 15:41:07 -0800

    crane-utils: bump deps

--- pkgs/crane-utils/default.nix
+++ pkgs/crane-utils/default.nix
@@ -8,5 +8,5 @@ rustPlatform.buildRustPackage {
 
   src = lib.sourceFilesBySuffices ./. [ ".rs" ".toml" ".lock" ];
 
-  cargoSha256 = "sha256-/ybcjLrjLBuL8jYN0thBQtqyrpKDPAltF9sRqc98Yw0=";
+  cargoHash = "sha256-b/l0GV2QCHntdMFFkAtsPjnVMAFUC++2J4VnIyavf6w=";
 }
