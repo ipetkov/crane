fa01f90b65278a30e336a41f2035d2b2ba2ede0d
Ivan Petkov <ivanppetkov@gmail.com>
2024-03-27 17:28:00 -0700

    Fix typo

--- extra-tests/alt-store/test.sh
+++ extra-tests/alt-store/test.sh
@@ -8,7 +8,8 @@ cd "${scriptDir}"
 case "$(nix --version)" in
   "nix (Nix) 2.21.0" | "nix (Nix) 2.21.1")
     echo 'skipping test: https://github.com/NixOS/nix/issues/10267'
+    ;;
 
-  *)
-  nix build .#default --override-input crane ../.. --store $(pwd)/alt-store
+  *) nix build .#default --override-input crane ../.. --store $(pwd)/alt-store
+    ;;
 esac
