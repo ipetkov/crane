2ed1e3f79666009838d5213f68de41679407f131
Ivan Petkov <ivanppetkov@gmail.com>
2024-05-25 16:21:11 -0700

    Don't use a tempfile

--- lib/setupHooks/removeReferencesToVendoredSourcesHook.sh
+++ lib/setupHooks/removeReferencesToVendoredSourcesHook.sh
@@ -4,34 +4,32 @@ removeReferencesToVendoredSources() {
   local installLocation="${1:-${out:?not defined}}"
   local vendoredDir="${2:-${cargoVendorDir:?not defined}}"
 
-  local sedScript="$(mktemp removeReferencesScriptXXXX)"
-  (
-    echo -n 's!@storeDir@/\(eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
-
-    (
-      # Include the root of the vendor dir itself
-      echo "${vendoredDir}"
-
-      # Include the individual crates themselves in case
-      # something else slips in a reference to them
-      find -L "${vendoredDir}" -mindepth 1 -maxdepth 1 -type d | \
-        xargs -I DIR find -H DIR -type l -exec readlink '{}' \;
-    ) |
-      grep --only-matching '@storeDir@/[a-z0-9]\{32\}' |
-      while read crateSource; do
-        echo -n '\|'"${crateSource#@storeDir@/}";
-      done
-
-    echo -n '\)!@storeDir@/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!g'
-  ) >"${sedScript}"
-
   (
     exec 3>&1
     echo stripping references to cargoVendorDir from:
     find "${installLocation}" -type f |
       sort |
       tee -a /dev/fd/3 |
-      xargs --no-run-if-empty sed -i'' -f "${sedScript}"
+      xargs --no-run-if-empty sed -i'' -f <(
+        echo -n 's!@storeDir@/\(eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+
+        (
+          # Include the root of the vendor dir itself
+          echo "${vendoredDir}"
+
+          # Include the individual crates themselves in case
+          # something else slips in a reference to them
+          find -L "${vendoredDir}" -mindepth 1 -maxdepth 1 -type d | \
+            xargs -I DIR find -H DIR -type l -exec readlink '{}' \;
+        ) |
+          grep --only-matching '@storeDir@/[a-z0-9]\{32\}' |
+          while read crateSource; do
+            echo -n '\|'"${crateSource#@storeDir@/}";
+          done
+
+        echo -n '\)!@storeDir@/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!g'
+      )
+
     echo stripping references done
   )
 
