ebac82086b9ab8a7879bb69462415ccf7b8834db
Ivan Petkov <ivanppetkov@gmail.com>
2024-05-25 14:56:25 -0700

    removeReferencesToVendoredSources: cache vendorDir crawling
    There is no reason to repeatedly crawl the vendor directories for each
    installed file, we can easily cache the results once up front.

--- lib/setupHooks/removeReferencesToVendoredSourcesHook.sh
+++ lib/setupHooks/removeReferencesToVendoredSourcesHook.sh
@@ -4,31 +4,34 @@ removeReferencesToVendoredSources() {
   local installLocation="${1:-${out:?not defined}}"
   local vendoredDir="${2:-${cargoVendorDir:?not defined}}"
 
-  local installedFile
-  while read installedFile; do
-    echo stripping references to cargoVendorDir from "${installedFile}"
-    time sed -i'' "${installedFile}" -f <(
-      echo -n 's!@storeDir@/\(eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+  local sedScript="$(mktemp removeReferencesScriptXXXX)"
+  (
+    echo -n 's!@storeDir@/\(eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 
+    (
+      # Include the root of the vendor dir itself
+      echo "${vendoredDir}"
+
+      # Include the individual crates themselves in case
+      # something else slips in a reference to them
+      find -L "${vendoredDir}" -mindepth 1 -maxdepth 1 -type d | \
+        xargs -I DIR find -H DIR -type l -exec readlink '{}' \;
+    ) |
+      grep --only-matching '@storeDir@/[a-z0-9]\{32\}' |
       while read crateSource; do
         echo -n '\|'"${crateSource#@storeDir@/}";
-      done < <(
-        (
-          # Include the root of the vendor dir itself
-          echo "${vendoredDir}"
+      done
 
-          # Include the individual crates themselves in case
-          # something else slips in a reference to them
-          find -L "${vendoredDir}" -mindepth 1 -maxdepth 1 -type d | \
-            xargs -I DIR find -H DIR -type l -exec readlink '{}' \;
-        ) | grep --only-matching '@storeDir@/[a-z0-9]\{32\}'
-      )
+    echo -n '\)!@storeDir@/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!g'
+  ) >"${sedScript}"
 
-      echo -n '\)!@storeDir@/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!g'
-    )
+  local installedFile
+  find "${installLocation}" -type f | while read installedFile; do
+    echo stripping references to cargoVendorDir from "${installedFile}"
+    sed -i'' "${installedFile}" -f "${sedScript}"
 
     @signIfRequired@
-  done < <(find "${installLocation}" -type f)
+  done
 }
 
 @sourceSigningUtils@
