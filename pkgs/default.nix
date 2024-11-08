5ba2e3643a4708aed902c2561461ab7f21236c33
Ivan Petkov <ivanppetkov@gmail.com>
2024-11-08 12:09:10 -0800

    Simplify docs fileset

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,20 +4,14 @@
   book =
     let
       inherit (pkgs) lib;
-      root = myLib.path ./..;
-      rootPrefix = toString root;
-      cleanedSrc = lib.cleanSourceWith {
-        src = root;
-        filter = path: _:
-          let
-            relativePath = lib.removePrefix rootPrefix path;
-          in
-          lib.any (prefix: lib.hasPrefix prefix relativePath) [
-            "/docs" # Build the docs directory
-            "/examples" # But also include examples as we cross-reference them
-            "/README.md"
-            "/CHANGELOG.md"
-          ];
+      cleanedSrc = lib.fileset.toSource {
+        root = ./..;
+        fileset = lib.fileset.unions [
+          ./../docs
+          ./../examples
+          ./../README.md
+          ./../CHANGELOG.md
+        ];
       };
     in
     pkgs.runCommand "crane-book" { } ''
