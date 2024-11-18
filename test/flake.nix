30ad43e875247c95349a301530afa860ae1a2073
Ivan Petkov <ivanppetkov@gmail.com>
2024-11-18 13:20:45 -0800

    examples: drop nixpkgs-for-wasm-bindgen
    The examples were already hard coding a concrete version of
    wasm-bindgen-cli so having the extra input was both useless and
    confusing

--- test/flake.nix
+++ test/flake.nix
@@ -4,10 +4,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-latest-release.url = "github:NixOS/nixpkgs/release-24.05";
 
-    # The version of wasm-bindgen-cli needs to match the version in Cargo.lock
-    # Update this to include the version you need
-    nixpkgs-for-wasm-bindgen.url = "github:NixOS/nixpkgs/4e6868b1aa3766ab1de169922bb3826143941973";
-
     advisory-db = {
       url = "github:rustsec/advisory-db";
       flake = false;
