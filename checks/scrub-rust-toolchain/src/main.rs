35309874d08880bf566b387ad5352c01f3bfc580
AdamDayan <adam.dayan@helsing.ai>
2025-01-06 19:57:40 +0000

    Add comment to scrub-rust-toolchain/main.rs

--- checks/scrub-rust-toolchain/src/main.rs
+++ checks/scrub-rust-toolchain/src/main.rs
@@ -1,5 +1,9 @@
 use std::cell::Cell;
 
+// the stdlib thread local includes a panic message. After Rust 1.83.0, this panic message includes
+// the full path to the stdlib if the toolchain has access to the Rust src. Nix takes the full path
+// to mean that the rust toolchain should be added to the runtime closure. We can test this
+// behaviour with  the below
 thread_local! {
     pub static FOO: Cell<u32> = Cell::new(1);
 }
