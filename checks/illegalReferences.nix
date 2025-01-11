aa367f612d086d2a6a88a1b70c006ea0df4c4ac2
Ivan Petkov <ivanppetkov@gmail.com>
2025-01-10 19:12:47 -0800

    DRY tests

--- checks/illegalReferences.nix
+++ checks/illegalReferences.nix
@@ -8,6 +8,31 @@
 let
   inherit (pkgs.stdenv) isDarwin;
 
+  extraAllowed = builtins.concatStringsSep "\\|" (lib.optionals isDarwin [
+    ""
+    "libiconv"
+    "libcxx"
+    "apple-framework-CoreFoundation"
+  ]);
+
+  checkRefs = name: crate: pkgs.runCommand
+    name
+    {
+      nativeBuildInputs = [ pkgs.binutils-unwrapped ];
+    } ''
+    for f in "$(find ${crate}/bin -type f)"; do
+      if strings "$f" | \
+        grep --only-matching '${builtins.storeDir}/[^/]\+' | \
+        grep --invert-match 'glibc\|gcc${extraAllowed}' | \
+        grep --invert-match '${builtins.storeDir}/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' --count
+      then
+        echo found references to /nix/store sources
+        false
+      fi
+    done
+    touch $out
+  '';
+
   # addresses https://github.com/ipetkov/crane/issues/773
   removeReferencesToRustToolchain =
     let
@@ -18,97 +43,38 @@ let
           "rustc"
         ];
       });
-
-      crate = myLibFatToolchain.buildPackage {
-        pname = "test-scrub-toolchain";
-        version = "0.1.0";
-        src = ./includes-toolchain-refs;
-        nativeBuildInputs = [ ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
-          pkgs.gcc10
-        ];
-      };
     in
-    pkgs.runCommand "test_removeReferencesToRustToolchain"
-      {
-        nativeBuildInputs = [ pkgs.binutils-unwrapped ];
-      } ''
-      if strings ${crate}/bin/test_crane | \
-        grep --only-matching '${builtins.storeDir}/[^/]\+' | \
-        grep --invert-match 'glibc\|gcc' | \
-        grep --invert-match '${builtins.storeDir}/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' --count
-      then
-        echo found references to /nix/store sources
-        false
-      else
-        touch $out
-      fi
-    '';
+    checkRefs "test_removeReferencesToRustToolchain" (myLibFatToolchain.buildPackage {
+      pname = "test-scrub-toolchain";
+      version = "0.1.0";
+      src = ./includes-toolchain-refs;
+      nativeBuildInputs = [ ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
+        pkgs.gcc10
+      ];
+    });
 
   # addresses https://github.com/ipetkov/crane/issues/773
-  removeReferencesToRustToolchainFenix =
-    let
-      crate = myLibFenix.buildPackage {
-        pname = "test-scrub-toolchain-fenix";
-        version = "0.1.0";
-        src = ./includes-toolchain-refs;
-        nativeBuildInputs = [ ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
-          pkgs.gcc10
-        ];
-      };
-    in
-    pkgs.runCommand "test_removeReferencesToRustToolchainFenix"
-      {
-        nativeBuildInputs = [ pkgs.binutils-unwrapped ];
-      } ''
-      if strings ${crate}/bin/test_crane | \
-        grep --only-matching '${builtins.storeDir}/[^/]\+' | \
-        grep --invert-match 'glibc\|gcc' | \
-        grep --invert-match '${builtins.storeDir}/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' --count
-      then
-        echo found references to /nix/store sources
-        false
-      else
-        touch $out
-      fi
-    '';
+  removeReferencesToRustToolchainFenix = checkRefs "test_removeReferencesToRustToolchainFenix" (myLibFenix.buildPackage {
+    pname = "test-scrub-toolchain-fenix";
+    version = "0.1.0";
+    src = ./includes-toolchain-refs;
+    nativeBuildInputs = [ ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
+      pkgs.gcc10
+    ];
+  });
 
   # https://github.com/ipetkov/crane/issues/119
-  removeReferencesToVendorDirAndCrates =
-    let
-      crate = myLib.buildPackage {
-        src = ./grpcio-test;
-        nativeBuildInputs = [
-          pkgs.cmake
-        ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
-          pkgs.gcc10
-        ];
-        buildInputs = lib.optionals isDarwin [
-          pkgs.libiconv
-        ];
-      };
-
-      extraAllowed = builtins.concatStringsSep "\\|" (lib.optionals isDarwin [
-        ""
-        "libiconv"
-        "libcxx"
-        "apple-framework-CoreFoundation"
-      ]);
-    in
-    pkgs.runCommand "removeReferencesToVendorDir"
-      {
-        nativeBuildInputs = [ pkgs.binutils-unwrapped ];
-      } ''
-      if strings ${crate}/bin/grpcio-test | \
-        grep --only-matching '${builtins.storeDir}/[^/]\+' | \
-        grep --invert-match 'glibc\|gcc${extraAllowed}' | \
-        grep --invert-match '${builtins.storeDir}/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' --count
-      then
-        echo found references to /nix/store sources
-        false
-      else
-        touch $out
-      fi
-    '';
+  removeReferencesToVendorDirAndCrates = checkRefs "removeReferencesToVendorDir" (myLib.buildPackage {
+    src = ./grpcio-test;
+    nativeBuildInputs = [
+      pkgs.cmake
+    ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
+      pkgs.gcc10
+    ];
+    buildInputs = lib.optionals isDarwin [
+      pkgs.libiconv
+    ];
+  });
 in
 linkFarmFromDrvs "illegalReferencesTests" [
   removeReferencesToRustToolchain
