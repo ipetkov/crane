a40947fa6445b8299909f93c37d6f42df9b906fa
Ivan Petkov <ivanppetkov@gmail.com>
2025-02-08 19:59:36 +0000

    checks: move all tests to the test flake (#791)

--- test.sh
+++ test.sh
@@ -2,17 +2,19 @@
 set -euo pipefail
 
 gitRoot="$(git rev-parse --show-toplevel)"
+cd "${gitRoot}"
 
 function flakeCheck() {
   if which nom >/dev/null 2>&1; then
     nix flake check --log-format internal-json -v -L "$@" |& nom --json
   else
-    nix flake check -L
+    nix flake check -L "$@"
   fi
 }
 
 flakeCheck
-flakeCheck --override-input nixpkgs "$("${gitRoot}/ci/ref-from-lock.sh" "${gitRoot}/test#nixpkgs-latest-release")"
+flakeCheck ./test#
+flakeCheck ./test# --override-input nixpkgs "$("${gitRoot}/ci/ref-from-lock.sh" "${gitRoot}/test#nixpkgs-latest-release")"
 
 for f in $(find ./examples -maxdepth 1 -mindepth 1 -type d | sort -u); do
   echo "validating ${f}"
