aa4576e72b215dc501aff742416d5d9ed9114224
Ivan Petkov <ivanppetkov@gmail.com>
2025-02-17 09:49:35 -0800

    Drop unused useFetchCargoVendor

--- pkgs/crane-utils/default.nix
+++ pkgs/crane-utils/default.nix
@@ -5,7 +5,6 @@
 rustPlatform.buildRustPackage {
   pname = "crane-utils";
   version = "0.0.1";
-  useFetchCargoVendor = true;
 
   src = lib.sourceFilesBySuffices ./. [ ".rs" ".toml" ".lock" ];
   cargoLock = { lockFile = ./Cargo.lock; };
