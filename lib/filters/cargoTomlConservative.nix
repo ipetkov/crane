8d6790ecd86ae4eab16eaf02f6db733acbf76cd8
Ivan Petkov <ivanppetkov@gmail.com>
2026-01-17 16:37:18 -0800

    Bring back some comments for the sake of auditing

--- lib/filters/cargoTomlConservative.nix
+++ lib/filters/cargoTomlConservative.nix
@@ -8,6 +8,17 @@ let
 
   pathsToRemovePerTarget = target: [
     # https://doc.rust-lang.org/cargo/reference/cargo-targets.html#configuring-a-target
+    # Additional package attributes which are expressly kept in
+    # (but listed here for audit purposes)
+    # "crate-type"        # some tools may try to inspect crate types (e.g. wasm-pack), retain the
+    #                     # definition to honor the project structure
+    # "edition"           # Influences cargo behavior
+    # "harness"           # Controls how tests are compiled and run, which might have implications
+    #                     # on additional scripts which try to run the tests during buildDepsOnly
+    # "name"              # let cargo manage targets/collisions/etc.
+    # "path"              # maintain project structure
+    # "proc-macro"        # If we have a proc-macro dependency in the workspace, rustc may try to
+    #                     # compile `proc-macro2` for the target system
     [target "test"]
     [target "doctest"]
     [target "bench"]
@@ -21,7 +32,15 @@ let
   pathsToRemove = [
     ["badges"] # Badges to display on a registry, not relevant for build
     ["lints"]  # Only applied to local sources, which we need to rebuild anyway
+
     # https://doc.rust-lang.org/cargo/reference/manifest.html#the-package-section
+    # Additional package attributes which are expressly kept in
+    # (but listed here for audit purposes)
+    # "edition"      # Influences cargo behavior
+    # "name"         # Required
+    # "resolver"     # Influences cargo behavior when edition != 2021
+    # "version"      # Required
+    # "workspace"    # Keep project structure as is
     ["package" "authors"]
     ["package" "autobenches"]
     ["package" "autobins"]
@@ -47,7 +66,16 @@ let
     ["package" "repository"]
     ["package" "rust-version"]
     ["package" "rust_version"]
+
     # https://doc.rust-lang.org/cargo/reference/workspaces.html
+    # Additional package attributes which are expressly kept in
+    # (but listed here for audit purposes)
+    # "default-members"
+    # "dependencies"
+    # "exclude"
+    # "members"
+    # "package"
+    # "resolver"
     ["workspace" "lints"]    # Only applied to local sources, which we need to rebuild anyway
     ["workspace" "metadata"] # Metadata generally not relevant for build except for specific tools
   ]
