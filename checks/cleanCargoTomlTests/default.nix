c87c85e9b06e86e6c1f1b25823d5bc83448c52a7
Benedikt Schaber <benedikt@schaber.io>
2026-01-24 21:37:56 +0100

    fix: cargoTomlAggressive only keeps top-level keys (#960)

--- checks/cleanCargoTomlTests/default.nix
+++ checks/cleanCargoTomlTests/default.nix
@@ -1,5 +1,6 @@
 {
   cleanCargoToml,
+  filters,
   lib,
   linkFarmFromDrvs,
   remarshal,
@@ -9,20 +10,22 @@
 
 let
   cmpCleanCargoToml =
-    name: path:
+    folderName: path: filterName: filter:
     let
       cleaned = cleanCargoToml {
         cargoToml = path + "/Cargo.toml";
+        cleanCargoTomlFilter = filter;
       };
-      cleanedToml = writeTOML "cleaned.toml" cleaned;
-      expected = path + "/expected.toml";
-
+      cleanedToml = writeTOML "cleaned-${folderName}-${filterName}.toml" cleaned;
+      expectedPathSpecific = path + "/expected-${filterName}.toml";
+      expected =
+        if lib.pathExists expectedPathSpecific then expectedPathSpecific else path + "/expected.toml";
       # 23.05 has remarshal 0.14 which sorts keys by default
       # starting with version 0.16 ordering is preserved unless
       # --sort-keys is specified
       sortKeys = lib.optionalString (lib.strings.versionAtLeast remarshal.version "0.16.0") "--sort-keys";
     in
-    runCommand "compare-${name}" { } ''
+    runCommand "compare-${folderName}-${filterName}" { } ''
       function reformat {
         ${remarshal}/bin/remarshal ${sortKeys} -i "$1" --of toml
       }
@@ -30,12 +33,17 @@ let
       diff <(reformat ${expected}) <(reformat ${cleanedToml})
       touch $out
     '';
+  cmpAllFilters =
+    name: path:
+    lib.mapAttrsToList (filterName: filter: cmpCleanCargoToml name path filterName filter) filters;
+
+  testMatrix = {
+    barebones = ./barebones;
+    complex = ./complex;
+    # https://github.com/ipetkov/crane/issues/610
+    complex-underscores = ./complex-underscores;
+    # https://github.com/ipetkov/crane/issues/800
+    proc-macro = ./proc-macro;
+  };
 in
-linkFarmFromDrvs "cleanCargoToml" [
-  (cmpCleanCargoToml "barebones" ./barebones)
-  (cmpCleanCargoToml "complex" ./complex)
-  # https://github.com/ipetkov/crane/issues/610
-  (cmpCleanCargoToml "complex-underscores" ./complex-underscores)
-  #https://github.com/ipetkov/crane/issues/800
-  (cmpCleanCargoToml "proc-macro" ./proc-macro)
-]
+linkFarmFromDrvs "cleanCargoToml" (lib.concatLists (lib.mapAttrsToList cmpAllFilters testMatrix))
