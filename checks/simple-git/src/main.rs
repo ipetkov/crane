5a70bd9112507fc4480ab07b8d0b2ed70c5a941e
Ivan Petkov <ivanppetkov@gmail.com>
2026-02-22 12:44:44 -0800

    checks: workspace-git: replace with a lighter reproduction

--- checks/simple-git/src/main.rs
+++ checks/simple-git/src/main.rs
@@ -5,7 +5,6 @@ const CHANNEL: &str = "nightly";
 
 fn main() {
     println!("{:?}", std::any::TypeId::of::<byteorder::LittleEndian>());
-    println!("{:?}", std::any::TypeId::of::<libc::c_int>());
     println!("{}: {}", CHANNEL, num_cpus::get());
     crane_test_repo::print();
 }
