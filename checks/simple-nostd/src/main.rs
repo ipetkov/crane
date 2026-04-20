493dfb117309d3a4e8f896030b51cf67066b7fc9
Elliot Berman <eberman@anduril.com>
2026-04-20 11:04:01 -0700

    checks: uefi: init
    Add a check to cross-compile simple UEFI application. UEFI std support
    is ongoing, so create a no_std package.

---
+++ checks/simple-nostd/src/main.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(any(target_os = "none", target_os = "uefi", target_arch = "amdgpu"), no_std)]
+#![cfg_attr(any(target_os = "none", target_os = "uefi", target_arch = "amdgpu"), no_main)]
+
+#[allow(unused_extern_crates)]
+extern crate core;
+
+#[cfg_attr(any(target_os = "none", target_os = "uefi", target_arch = "amdgpu"), panic_handler)]
+fn panic(_info: &::core::panic::PanicInfo<'_>) -> ! {
+    loop {}
+}
+
+#[cfg_attr(target_os = "uefi", unsafe(export_name = "efi_main"))]
+fn main() { }
