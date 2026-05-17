a2d2c5ac47baf4b9787e790f9f7c11d92e9e3cdc
Ivan Petkov <ivanppetkov@gmail.com>
2026-05-17 10:21:48 -0700

    craneUtils: use writableTmpDirAsHomeHook

--- pkgs/crane-utils/default.nix
+++ pkgs/crane-utils/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   rustPlatform,
+  writableTmpDirAsHomeHook,
 }:
 
 rustPlatform.buildRustPackage {
@@ -13,4 +14,14 @@ rustPlatform.buildRustPackage {
     ".lock"
   ];
   cargoLock.lockFile = ./Cargo.lock;
+
+  # NB: buildRustPackage doesn't set a CARGO_HOME location by default
+  # which means it falls back on $HOME/.cargo (which points to a non-existent
+  # /homeless-shelter/.cargo by default). On non-sandboxed builds this will
+  # cause errors as Nix does not expect `/homeless-shelter` to exist. Hence
+  # we ensure HOME is set to some writable path within the current build
+  # https://github.com/ipetkov/crane/issues/815
+  nativeBuildInputs = [
+    writableTmpDirAsHomeHook
+  ];
 }
