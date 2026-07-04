80db5bdc391be8a1794f6d8a2d56e3a84ebcede2
Ivan Petkov <ivanppetkov@gmail.com>
2026-07-04 15:10:18 -0700

    Drop support for nixpkgs-25.11 (#1056)

--- extra-tests/dummy-does-not-depend-on-flake-source-via-path/test.sh
+++ extra-tests/dummy-does-not-depend-on-flake-source-via-path/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -eu
 
@@ -6,13 +6,13 @@ scriptDir=$(dirname "$0")
 cd "${scriptDir}"
 
 nixpkgsOverride="$(../../ci/ref-from-lock.sh ../../test#nixpkgs)"
-craneOverride="--override-input crane ../.. --override-input nixpkgs ${nixpkgsOverride}"
-flakeSrc=$(nix flake metadata ${craneOverride} --json 2>/dev/null | jq -r '.path')
+craneOverride=(--override-input crane ../.. --override-input nixpkgs "${nixpkgsOverride}")
+flakeSrc=$(nix flake metadata "${craneOverride[@]}" --json 2>/dev/null | jq -r '.path')
 
 # Get information about the default derivation
 # Then pull out any input sources
-drvSrcs=$(nix show-derivation ${craneOverride} '.#dummy' 2>/dev/null |
-  jq -r 'to_entries[].value.inputSrcs[]')
+drvSrcs=$(nix show-derivation "${craneOverride[@]}" '.#dummy' 2>/dev/null |
+  jq -r '.derivations | to_entries[].value.inputs.srcs[]')
 
 # And lastly make sure we DO NOT find the flake root source listed
 # or else the dummy derivation will depend on _too much_ (and get
